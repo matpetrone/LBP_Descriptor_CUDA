7b4abdc3d69c4dd0268764f3d45cb7018286dbb6
francesca <francidl96@gmail.com>
2021-01-12 12:37:40 +0100

    main cuda device properties

--- src/main.cu
+++ src/main.cu
@@ -2,5 +2,10 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, 0); // 0-th device
+    std::cout << "num SM: " << deviceProp.multiProcessorCount << std::endl;
+    std::cout << "Max threads per block: " << deviceProp.maxThreadsPerBlock << std::endl;
+    std::cout << "Shared memory per block: " << deviceProp.sharedMemPerBlock << std::endl;
     return 0;
 }
